26bec3b3e2cd8e100f8036185ff8f250e01f88c7
spendlively <spendlively@mail.ru>
2019-07-26 14:14:32 +0700

    osm loading message

--- rsdu/src/js/index.js
+++ rsdu/src/js/index.js
@@ -3,13 +3,11 @@ import {init as map} from "./map/map"
 import {init as l10n} from "./l10n/init"
 import {init as search} from "./search/search"
 import {init as table} from "./table/table"
-// import {init as websocket} from "./websocket/client"
-// import {init as socketio} from "./websocket/socketio"
+
+console.log('OMS Loading...')
 
 l10n()
 auth()
 map()
 search()
 table()
-// websocket()
-// socketio()
